7daed9f837d9afcaff2211b6452e4e3874062ab4
codervivek007 <110727969+codervivek007@users.noreply.github.com>
2022-12-12 15:55:35 +0530

    Add files via upload
    Done

---
+++ script.js
@@ -0,0 +1,28 @@
+// var x = "aabccfgg"
+
+// function reverseString(x) {
+//  var s = "" 
+//  for ( var i = x.length-1; i>=0; i--){
+//     s=s+x[i];
+//  }
+//  console.log(s);
+// }
+
+// console.log(x);
+
+// reverseString(x);
+
+
+const arr = [2,3,4,5,"a"]
+
+// function reverseArray (arr) {
+//     var arr1 = []
+//     for ( var i = arr.length-1; i>=0; i--){
+//         arr1.push(arr[i])
+//     }
+//     console.log(arr1);
+// }
+// reverseArray(arr);
+
+
+ 
